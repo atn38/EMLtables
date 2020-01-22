232fdc05dff47db6d6173967110c6eb2e37d40e9
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2020-01-22 14:15:03 -0600

    prelim get_parties

---
+++ R/get_parties.R
@@ -0,0 +1,102 @@
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+
+
+get_parties <- function(corpus) {
+  party_list <- list()
+  vw_parties <- data.frame()
+
+  # loop through each EML doc in corpus
+  for (i in seq_along(corpus)) {
+    scope <- sub("\\..*$", "", names(corpus)[[i]])
+
+    # get string between the two periods -- datasetid
+    id <- str_extract(names(corpus), "(?<=\\.)(.+)(?=\\.)")[[i]]
+
+    # get string after last period -- revision
+
+    rev <- sub(".*\\.", "", names(corpus))[[i]]
+
+    parties <-
+      purrr::compact(corpus[[i]][["dataset"]][c("creator",
+                                                "associatedParty",
+                                                "contact",
+                                                "publisher",
+                                                "metadataProvider")])
+
+    # party_list <- c(party_list, parties)
+
+    for (j in seq_along(parties)) {
+      party <- parties[[j]]
+      if (!is.null(names(party)))
+        party <- list(party)
+
+      for (k in seq_along(party)) {
+        entity <- party[[k]]
+
+        # get authorship order if creator
+        if (names(parties)[[j]] == "creator")
+          order <- paste0(j, k)
+        else
+          order <- NA
+
+        # get role if not associatedparty
+        if (names(parties)[[j]] != "associatedParty")
+          role <- names(parties)[[j]]
+        else if (names(parties)[[j]] == "associatedParty")
+          role <- na_if_null(entity[["role"]])
+
+        first <-
+          na_if_null(entity[["individualName"]][["givenName"]])
+        sur <- na_if_null(entity[["individualName"]][["surName"]])
+        org <- na_if_null(entity[["organizationName"]])
+
+        address <-
+          na_if_null(paste(entity[["address"]][["deliveryPoint"]], collapse = ", "))
+        city <- na_if_null(entity[["address"]][["city"]])
+        state <-
+          na_if_null(entity[["address"]][["administrativeArea"]])
+        country <- na_if_null(entity[["address"]][["country"]])
+        zip <- na_if_null(entity[["address"]][["postalCode"]])
+        phone <- na_if_null(entity[["phone"]])
+        email <- na_if_null(entity[["electronicMailAddress"]])
+        web <- na_if_null(entity[["onlineUrl"]])
+
+        partydf <- data.frame(
+          scope = I(scope),
+          id = id,
+          rev = rev,
+          order = order,
+          role = I(role),
+          firstname = I(first),
+          surname = sur,
+          organization = org,
+          address1 = address,
+          city = city,
+          state = state,
+          country = country,
+          zip = zip,
+          phone = I(phone),
+          email = I(email),
+          online_url = I(web),
+          stringsAsFactors = F
+        )
+
+        vw_parties <- rbind(vw_parties, partydf)
+      }
+
+
+    }
+  }
+  return(vw_parties)
+}
