76604e308a00ce9b6eb18ec4435537f00a958ad2
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2020-01-27 14:34:03 -0600

    prelim get_attributes

---
+++ R/get_attributes.R
@@ -0,0 +1,53 @@
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#' only data table attributes for now
+
+get_attributes <- function(corpus) {
+  vw_att <- data.frame()
+
+  for (i in seq_along(corpus)) {
+    pk <- get_pk(names(corpus)[[i]])
+    scope <- pk[["scope"]]
+    id <- pk[["id"]]
+    rev <- pk[["rev"]]
+
+    dts <-
+      handle_one(purrr::compact(corpus[[i]][["dataset"]][["dataTable"]]))
+
+    for (j in seq_along(dts)) {
+      dt <- dts[[j]]
+
+      if (!is.null(dt[["attributeList"]])) {
+        attdf <- EML::get_attributes(x = dt[["attributeList"]],
+                                     eml = corpus[[i]])
+        how_many <- nrow(attdf[["attributes"]])
+        print(how_many)
+        attdf2 <- data.frame(
+          stringsAsFactors = F,
+          scope = rep(scope, how_many),
+          id = rep(id, how_many),
+          rev = rep(rev, how_many),
+          entityposition = rep(j, how_many)
+        )
+
+        print(str(attdf[["attributes"]]))
+
+        # attdf <- data.frame(attdf, attdf2)
+
+        vw_att <- rbind(vw_att, attdf[["attributes"]])
+      }
+    }
+
+  }
+  return(vw_att)
+}
