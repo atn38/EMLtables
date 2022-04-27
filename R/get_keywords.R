f58a323f7e5de51bd0b38b75e26d90231f067c11
atn38-crwr-d07322 <an.thai.nguyen.38@gmail.com>
2022-04-27 12:58:51 -0500

    fix: get_keywords to use get_datasetlevel_element instead #2 #9

--- R/get_keywords.R
+++ R/get_keywords.R
@@ -8,25 +8,7 @@
 #'
 #' @examples
 get_keywords <- function(corpus) {
-  vw <- list()
-  for (i in seq_along(corpus)) {
-    pk <- parse_packageId(names(corpus)[[i]])
-    scope <- pk[["scope"]]
-    id <- pk[["id"]]
-    rev <- pk[["rev"]]
-    klist <- list()
-    if ("keywordSet" %in% names(corpus[[i]][["dataset"]])) {
-      keysets <- handle_one(corpus[[i]][["dataset"]][["keywordSet"]])
-      klist <-
-        data.table::rbindlist(lapply(seq_along(keysets), function(x) parse_keywordset(keysets[[x]])), fill = TRUE)
-      klist$id <- id
-      klist$scope <- scope
-      klist$rev <- rev
-      klist <- subset(klist, select = c(4:6, 1:3))
-    }
-    vw[[i]] <- klist
-  }
-  return(data.table::rbindlist(vw))
+  get_datasetlevel_element(corpus, "keywordSet", parse_keywordset)
 }
 
 #' Title
