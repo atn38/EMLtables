a2340a9c1ecd6f20ec0145bc37899c1a5fb7db98
atn38-crwr-d07322 <an.thai.nguyen.38@gmail.com>
2022-05-18 21:02:29 -0500

    refactor get_entities #2

--- R/get_entities.R
+++ R/get_entities.R
@@ -28,37 +28,30 @@ get_entities <- function(corpus) {
     if (is.null(ent_groups))
       return()
     entgroupdf <- list()
-    # attgroupdf <- list()
     # loop through each entity group
     for (j in seq_along(ent_groups)) {
       ents <- handle_one(ent_groups[[j]])
 
       # loop through each entity in each entity group
       ent_list <- lapply(seq_along(ents), function(x) {
-
-        info <- data.frame(scope = scope,
-        id = id,
-        rev = rev,
-        entity = paste0(j, x),
-        entitytype = paste0(names(ent_groups)[[j]]),
-        stringsAsFactors = F)
+        info <- data.frame(
+          scope = scope,
+          datasetid = id,
+          rev = rev,
+          entity = paste0(j, x),
+          entitytype = paste0(names(ent_groups)[[j]]),
+          stringsAsFactors = F
+        )
 
         entdf2 <- parse_entity(ents[[x]])
-
-        # # get attributes
-        # atts <- parse_attributeList(x = ents[[x]], eml = corpus[[i]])
-        # n <- nrow(atts)
-        # attdf <- cbind(info[rep(seq_len(nrow(info)), each = n), ], atts)
         return(cbind(info, entdf2))
       })
-      entgroupdf[[j]] <- data.table::rbindlist(ent_list, fill = TRUE)
-      # attgroupdf[[j]] <- data.table::rbindlist(ent_list[[att]], fill = TRUE)
+      entgroupdf[[j]] <-
+        data.table::rbindlist(ent_list, fill = TRUE)
     }
-    vw_entities[[i]] <- data.table::rbindlist(entgroupdf, fill = TRUE)
-    # vw_atts[[i]] <- data.table::rbindlist(attgroupdf, fill = TRUE)
+    vw_entities[[i]] <-
+      data.table::rbindlist(entgroupdf, fill = TRUE)
   }
-
-  return(data.table::rbindlist(vw_entities, fill = TRUE))
   out <- data.table::rbindlist(vw_entities, fill = TRUE)
   msgout(out)
   return(out)
@@ -74,9 +67,7 @@ get_entities <- function(corpus) {
 parse_entity <- function(ent) {
   data.frame(
     entityname = ent$entityName,
-    entitydescription = trimws(I(null2na(
-      ent$entityDescription
-    ))),
+    entitydescription = null2na(ent$entityDescription),
     nrow = null2na(ent$numberOfRecords),
     filename = null2na(ent$physical$objectName),
     filesize = null2na(ent$physical$size$size),
